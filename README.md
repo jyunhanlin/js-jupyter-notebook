afda4e2ddbbe63ac9c6882c05eafb99777836460
Hank Lin <jyunhanlin@gmail.com>
2021-03-20 12:09:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Setup and use
+
+### Install
+
+```sh
+npm install
+```
+
+### Development
+
+```sh
+npm start
+```
+
+## Tech stack
+
+- react
+- typescript
+- chakra-ui
---
+++ src/test-utils.tsx
@@ -0,0 +1,12 @@
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { render, RenderOptions } from '@testing-library/react';
+import React from 'react';
+
+const AllProviders = ({ children }: { children?: React.ReactNode }) => (
+  <ChakraProvider theme={theme}>{children}</ChakraProvider>
+);
+
+const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
+  render(ui, { wrapper: AllProviders, ...options });
+
+export { customRender as render };
